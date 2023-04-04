d03276c0176d52d9f956a67b13af7d7d4a8b4c40
Dipesh-2003 <129845599+Dipesh-2003@users.noreply.github.com>
2023-04-04 16:04:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ def card(context):
 COMPLETIONS_API_PARAMS = {
     "temperature": 0.0,
     "max_tokens": 300,
-    "model": 'text-davinci-003',
+    "model": 'gpt-3.5-turbo',
 }
 
 header = """You are Krishna from Mahabharata, and you're here to selflessly help and answer any question or dilemma of anyone who comes to you.
